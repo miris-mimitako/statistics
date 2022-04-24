1b88aa83e563a862238687380770f719aaf74e5f
miris-mimitako <miris.mikitako@gmail.com>
2022-04-24 13:35:03 +0900

    add: debug code

--- modules/view/htmlgenerator.py
+++ modules/view/htmlgenerator.py
@@ -2,34 +2,39 @@ import jinja2
 from jinja2 import Template, Environment, FileSystemLoader
 import json
 
+
 class HtmlGenerator():
     def __init__(self) -> None:
-        pass
+        self.params = {"parameter": [
+            {"title": "", "img_src": "", "comment": ""}]}
+
+    def add_params(self):
+        # debug only
+        self.params["parameter"][0]["title"] = "test_title"
+        self.params["parameter"][0]["img_src"] = r"../../results/tmp/20220422_test/mimitako.png"
+        self.params["parameter"][0]["comment"] = "test_comment"
+        ##E debug
 
     def html_writer(self):
         # Read template
-        env = Environment(loader=FileSystemLoader('template/statistics/', encoding='utf8'))
-        tmpl = env.get_template('test.j2') # set your templates
+        env = Environment(loader=FileSystemLoader(
+            'template/statistics/', encoding='utf8'))
+        tmpl = env.get_template('test.j2')  # set your templates
 
-
-        params = {
-            "shop_name": "雑貨屋",
-            "item": "お皿",
-            "price": "200"
-        }
+        NN = self.add_params() # debug only
 
         # rendering and output HTML
-        rendered_html = tmpl.render(params)
-        with open('results/html/result.html', 'w',encoding="utf-8") as f:
+        set_data = self.params["parameter"]
+        rendered_html = tmpl.render(set_data = set_data, html_title = "this title")
+        with open('results/html/result.html', 'w', encoding="utf-8") as f:
             f.write(rendered_html)
-
+        ##E def html_writer
 
     def __del__(self):
         pass
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     HG = HtmlGenerator()
     test = HG.html_writer()
     del HG
-
