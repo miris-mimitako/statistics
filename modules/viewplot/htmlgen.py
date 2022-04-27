7d449fe0849de9cb19f76fb949547e8842e68469
miris-mimitako <miris.mikitako@gmail.com>
2022-04-27 22:12:45 +0900

    modify: mkdir

--- modules/viewplot/htmlgen.py
+++ modules/viewplot/htmlgen.py
@@ -7,13 +7,14 @@ import datetime
 import re
 from pathlib import Path
 
-'''
-Rev: 0.0.3
-'''
 # debug
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+
+'''
+Rev: 0.0.4
+'''
 
 
 class HtmlGenerator:
@@ -24,7 +25,7 @@ class HtmlGenerator:
         self.make_directory_for_result()
 
     def make_directory_for_result(self):
-        base_directory = Path(__file__).resolve().parent.parent.parent
+        self.base_directory = Path(__file__).resolve().parent.parent.parent
         now_date = datetime.datetime.today()
         folder_name = str(now_date.date()) + "-" + \
             re.sub(r"[^a-zA-Z0-9]", "", self.project_title)
@@ -33,10 +34,10 @@ class HtmlGenerator:
         mkdir_counter = 0
         while flag_of_mkdir:
             try:
-                os.makedirs(os.path.join(base_directory, "results/tmp",
+                os.makedirs(os.path.join(self.base_directory, "results/tmp",
                             str(folder_name), str(mkdir_counter)))
                 flag_of_mkdir = False
-                self.directory_location = os.path.join(base_directory, "results/tmp",
+                self.directory_location = os.path.join(self.base_directory, "results/tmp",
                                                        str(folder_name), str(mkdir_counter))
                 print(self.directory_location)
                 self.relation_directory_location = os.path.join(
@@ -69,15 +70,21 @@ class HtmlGenerator:
 
     def html_writer(self):
         # Read template
-        env = Environment(loader=FileSystemLoader(
-            'template/statistics/', encoding='utf8'))
+        env = Environment(loader=FileSystemLoader(os.path.join(
+            self.base_directory, 'template/statistics/'), encoding='utf8'))
         tmpl = env.get_template('plotview.j2')  # set your templates
+
+        try:
+            os.makedirs(os.path.join(self.base_directory, "results/html"))
+        except:
+            pass
+        # E try
 
         # rendering and output HTML
         set_data = self.params["parameter"]
         rendered_html = tmpl.render(
             set_data=set_data, html_title=self.project_title)
-        with open('results/html/' + str(self.project_title) + "-" + str(self.dir_counter) + '.html', 'w', encoding="utf-8") as f:
+        with open(os.path.join(self.base_directory, "results/html")+"/" + str(self.project_title) + "-" + str(self.dir_counter) + '.html', 'w', encoding="utf-8") as f:
             f.write(rendered_html)
         # E def html_writer
 
